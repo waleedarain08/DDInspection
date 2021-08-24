93384767a75fec3a29ac6e2da08a02556ed3b545
Yousuf223 <yousuffareed471@gmail.com>
2021-08-24 13:06:25 +0500

    first cm

--- android/app/src/main/java/com/jugglingwarehouse/MainActivity.java
+++ android/app/src/main/java/com/jugglingwarehouse/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jugglingwarehouse;
+package com.ddinspection;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "JugglingWarehouse";
+    return "DDInspection";
   }
 }
--- android/app/src/main/java/com/jugglingwarehouse/MainApplication.java
+++ android/app/src/main/java/com/jugglingwarehouse/MainApplication.java
@@ -1,4 +1,4 @@
-package com.jugglingwarehouse;
+package com.ddinspection;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.jugglingwarehouse.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.ddinspection.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
