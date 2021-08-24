c93cf32c6de56264eb14c1d1faf272faba322e3f
asifali2002 <asif.bholim90@gmail.com>
2021-08-24 16:54:33 +0500

    signup screen work

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,216 +1,148 @@
-import { Text, View, StyleSheet, Image, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
-import React, { useState } from 'react';
-import { Input, Button } from 'react-native-elements';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { userLogin } from '../../redux/actions';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
+import React, {useState} from 'react';
+import {Input, Button} from 'react-native-elements';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {userLogin} from '../../redux/actions';
+import {TextInput} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scrollview'
 
-function Signup({ navigation, userInfo, userLogin }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+function Signup({navigation, userInfo, userLogin}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (
-        <SafeAreaView style={styles.safeArea}>
-            <ScrollView contentContainerStyle={styles.container}>
-            {/* <View style={{flex:1}}> */}
-                <View style={styles.box1}>
-                    <TouchableOpacity onPress={() => navigation.goBack()}>
-                        <Image style={styles.back} source={require('../../assets/back.png')} />
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.box2}>
-                    <Image style={styles.logo} source={require('../../assets/juggling.png')} />
-                    <Text style={styles.logindv}>SignUp</Text>
-                    <View style={styles.loginline}></View>
-                </View>
-                <View style={styles.box3}>
-                    <View>
-                        <Image style={styles.logo02} source={require('../../assets/user.png')} />
-                        <Input style={styles.email} placeholder="First Name" />
-                    </View>
-                    <View>
-                        <Image style={styles.logo02} source={require('../../assets/user.png')} />
-                        <Input style={styles.password} placeholder="Last Name" />
-                    </View>
-                    <View>
-                        <Image style={styles.logo022} source={require('../../assets/email.png')} />
-                        <Input style={styles.email} placeholder="Email Address" />
-                    </View>
-                    <View>
-                        <Image style={styles.logo02} source={require('../../assets/phone.png')} />
-                        <Input style={styles.password} placeholder="Phone No." />
-                    </View>
-                    <View>
-                        <Image style={styles.logo02} source={require('../../assets/lock.png')} />
-                        <Input style={styles.email} placeholder="Password" secureTextEntry={true} />
-                    </View>
-                    <View>
-                        <Image style={styles.logo02} source={require('../../assets/lock.png')} />
-                        <Input style={styles.password} placeholder="Comfirm Password" secureTextEntry={true} />
-                    </View>
-                </View>
-                <View style={styles.box4}>
-                    <TouchableOpacity
-                        activeOpacity={0.8}
-                        style={styles.LoginButton}
-                        onPress={() => navigation.goBack()}
-                    >
-                        <Text style={styles.LoginButtonInside}>SIGN UP</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.box5}>
-                    <View style={styles.loginhere}>
-                        <Text style={styles.account}> Already have an account</Text>
-                        <Text onPress={() => navigation.goBack()} style={styles.singup}>LOGIN HERE</Text>
-                    </View>
-                    <View style={styles.line}>
-                        <View style={styles.singupline}>
-                        </View>
-                    </View>
-                </View>
-                {/* </View> */}
-            </ScrollView>
-        </SafeAreaView>
-    );
+  return (
+    <View style={styles.MainContainer}>
+      <View style={styles.header}>
+        <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
+          SIGN UP
+        </Text>
+      </View>
+
+      <KeyboardAwareScrollView style={styles.footer}>
+        <View style={styles.inputContainer}>
+          <Input
+            style={styles.inputText}
+            labelStyle={styles.inputLabel}
+            label="First Name"
+            placeholder="First Name"
+          />
+          <Input
+            style={styles.inputText}
+            labelStyle={styles.inputLabel}
+            label="Last Name"
+            placeholder="Last Name"
+            
+          />
+
+          <Input
+            style={styles.inputText}
+            labelStyle={styles.inputLabel}
+            label="Phone No."
+            placeholder="Phone No."
+          />
+          <Input
+            style={styles.inputText}
+            labelStyle={styles.inputLabel}
+            label="Password"
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+          <Input
+            style={styles.inputText}
+            labelStyle={styles.inputLabel}
+            label="Confirm Password"
+            placeholder="Confirm Password"
+            secureTextEntry={true}
+          />
+
+          <View style={styles.ButtonContainer}>
+            <TouchableOpacity activeOpacity={0.8} style={styles.LoginButton}>
+              <Text style={styles.LoginButtonInside}>Sign Up</Text>
+            </TouchableOpacity>
+            <Text style={{fontSize: 12}}>I have an account? <Text style={{fontSize: 12, color:'#5b53ff'}}>Login now</Text></Text>
+          </View>
+        </View>
+      </KeyboardAwareScrollView>
+    </View>
+  );
 }
 
-
 const mapStateToProps = state => {
-    return { userInfo: state?.userInfo };
+  return {userInfo: state?.userInfo};
 };
 
 const mapDispatchToProps = dispatch =>
-    bindActionCreators({ userLogin }, dispatch);
+  bindActionCreators({userLogin}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Signup);
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-        backgroundColor: '#0e101f'
-    },
-    container: {
-        flexGrow:1,
-    },
-    logindv: {
-        fontSize: 13,
-        paddingTop: 30,
-        fontWeight: 'bold',
-        color: "white",
-        borderBottomColor: "white",
-    },
-    loginline: {
-        width: 24,
-        height: 1,
-        backgroundColor: "white",
-        marginTop: 10,
-        marginBottom: 10,
-    },
-    logo: {
-        height: 100,
-        width: 200,
-    },
-    back: {
-        height: 17,
-        width: 23,
+  MainContainer: {
+    flex: 1,
+    backgroundColor: '#5b53ff',
+  },
+  header: {
+    flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: '#ffffff',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    elevation: 10,
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingTop: 30,
+  },
+  inputText: {
+    paddingLeft: 20,
+    fontSize: 12,
+  },
+  inputLabel: {
+    color: '#5b6777',
+    paddingLeft: 20,
+    fontSize: 12,
+  },
 
-    },
-    box1: {
-        flex: .5,
-        paddingLeft: "5%",
-        paddingTop: "1%",
-    },
-    box2: {
-        flex: 3,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    email: {
-        paddingTop: 12,
-        paddingLeft: 35,
-        fontSize: 12,
-    },
-    box3: {
-        flex: 3,
-        paddingHorizontal: 27,
-    },
-    password: {
-        paddingTop: 12,
-        paddingLeft: 35,
-        fontSize: 12,
-    },
-    logo02: {
-        height: 14,
-        width: 10,
-        position: "absolute",
-        top: 16,
-        left: 20,
-    },
-    logo022: {
-        height: 12,
-        width: 12,
-        position: "absolute",
-        top: 17,
-        left: 18,
-    },
-    box4: {
-        flex: 0.5,
-        justifyContent:"center",
-    },
-    button: {
-        alignItems: "center",
-        backgroundColor: "#1974ba",
-        padding: 18,
-        textAlign: "center",
-        color: "#ffffff",
-        marginLeft: 36,
-        marginRight: 36,
-        fontWeight: 'bold',
-        fontSize: 16,
-    },
-    LoginButtonInside: {
-        color: "#ffffff",
-        fontWeight: "bold",
-        fontSize: 12,
-    },
-    LoginButton: {
-        alignItems: "center",
-        backgroundColor: "#1974ba",
-        padding: 16,
-        marginHorizontal: 40,
-        borderRadius: 3,
-    },
-    box5: {
-        flex: 1.2,
-        justifyContent:"center",
-        alignItems:"center",
-        paddingVertical:8,
-    },
-    account: {
-        textAlign: "center",
-        color: "#dedee0",
-        fontSize: 10,
-    },
-    singup: {
-        textAlign: "center",
-        color: "#1b73bd",
-        fontWeight: 'bold',
-        fontSize:12,
-    },
-    singupline: {
-        width: 82,
-        height: 2,
-        backgroundColor: "#17619c",
-    },
-    loginhere: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "center",
-    },
-    line: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "center",
-    }
-
-})
+  LoginButton: {
+    alignItems: 'center',
+    backgroundColor: '#5b53ff',
+    width: 320,
+    padding: 18,
+    borderRadius: 35,
+    marginBottom: 40,
+    elevation: 10,
+    // shadowColor: '#000',
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 2,
+    // },
+    // shadowOpacity: 10.5,
+    // shadowRadius: 5.84,
+  },
+  LoginButtonInside: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  ButtonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+});
