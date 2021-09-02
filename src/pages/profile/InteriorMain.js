5af7dae5b7ac0ab0c366985f242251c9c375759b
asifali2002 <asif.bholim90@gmail.com>
2021-09-02 18:44:57 +0500

    interiorMainAsifWork

--- src/pages/profile/InteriorMain.js
+++ src/pages/profile/InteriorMain.js
@@ -10,17 +10,24 @@ import {
     ScrollView,
     TouchableOpacity,
 } from 'react-native';
+import Modal from 'react-native-modal';
 import * as Animatable from 'react-native-animatable';
 import { Input, Button, Card, SearchBar } from 'react-native-elements';
 
 export default function InteriorMain({ navigation }) {
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
         <ScrollView contentContainerStyle={{ height: 900, backgroundColor: "#ffffff" }}>
             <Text style={{ paddingHorizontal: 10, paddingVertical: 20, color: "#5a5d62" }}>Please Confirm You Are Weraing Booties Before Intering The Interior.</Text>
+            <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => setModalVisible(true)}>
             <View style={styles.bathroom}>
                 <Text style={{ color: "#2b3036", fontWeight: "bold" }}>Bathroom 1</Text>
                 <Text>+</Text>
             </View>
+            </TouchableOpacity>
             <View style={styles.bathroom}>
                 <Text style={{ color: "#2b3036", fontWeight: "bold" }}>Bathroom 2</Text>
                 <Text>+</Text>
@@ -76,6 +83,52 @@ export default function InteriorMain({ navigation }) {
             <View style={styles.completeButton}>
                 <Text style={{textAlign:"center",color:"#ffffff"}}>Complete</Text>
             </View>
+
+            <Modal
+          animationType="slideInUp"
+          transparent={true}
+          isVisible={modalVisible}
+          swipeDirection="down"
+          style={styles.view}
+          onSwipeComplete={() => setModalVisible(false)}
+          onBackButtonPress={() => setModalVisible(!modalVisible)}
+          onBackdropPress={() => setModalVisible(!modalVisible)}>
+          <View style={styles.modalView}>
+            <TouchableOpacity
+              activeOpacity={0.9}
+              hitSlop={{top: 20, bottom: 20, left: 40, right: 40}}>
+              {/* // onPress={() => setModalVisible(!modalVisible)}> */}
+              <View style={styles.modalLineView}></View>
+            </TouchableOpacity>
+
+            <View style={{marginVertical: 25}}>
+              <TouchableOpacity
+                activeOpacity={0.9}
+                onPress={() => navigation.navigate('')}
+                style={styles.modalButtons}>
+                <Text style={{color: '#fff'}}>Cannot Access Property</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.9}
+                onPress={() => navigation.navigate('')}
+                style={styles.modalButtons}>
+                <Text style={{color: '#fff'}}>Utilities Not Activated</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.9}
+                onPress={() => navigation.navigate('DoNotBuy')}
+                style={styles.modalButtons}>
+                <Text style={{color: '#fff'}}>Do Not Buy</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.9}
+                onPress={() => navigation.navigate('')}
+                style={styles.modalButtons}>
+                <Text style={{color: '#fff'}}>Pause Inspection</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
         </ScrollView>
     );
 }
@@ -164,6 +217,43 @@ const styles = StyleSheet.create({
         width:12,
         height:12,
         marginTop:5
-    }
+    },
+
+
+ 
+      modalView: {
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+      },
+      modalLineView: {
+        width: 35,
+        height: 2,
+        backgroundColor: '#8b98a8',
+        marginTop: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 2,
+      },
+    
+      modalButtons: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#193250',
+        paddingVertical: '4%',
+        paddingHorizontal: '25%',
+        margin: '3%',
+        borderRadius: 25,
+      },
 
 })
