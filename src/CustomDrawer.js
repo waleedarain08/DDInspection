df5017657b61f45eb5bf5aa5eaf0463177f919ed
asifali2002 <asif.bholim90@gmail.com>
2021-09-17 15:26:42 +0500

    custom Drawer work

--- src/CustomDrawer.js
+++ src/CustomDrawer.js
@@ -21,42 +21,63 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 
-function CustomDrawer({navigation,userLogout}) {
+function CustomDrawer({navigation,userLogout,route}) {
 
     logout = () => {
         navigation.closeDrawer();
         userLogout(null);
     }
 
+
+    console.log(route)
+    console.log(navigation)
+
     return (
-        <View style={{ flex: 1,}}>
-            <View style={{flex:1.3,backgroundColor:"#193250",justifyContent:"center",alignItems:"center"}}>
+        <View style={{ flex: 1, backgroundColor:"#fff"}}>
+            <View style={{flex:1.3,backgroundColor:"#193250",justifyContent:"center",alignItems:"center", paddingVertical:15}}>
             <Image style={styles.logo} source={require('./assets/sylvanRoad.png')} />
             </View>
             <View style={styles.manu}>
-                <ButtonView activeOpacity={0.8} style={{flex:0.8,flexDirection:"row",alignItems:"center"}}>
-                <Image style={styles.homeicon} source={require('./assets/homeIcon2.png')} style={{height:18, width:18, resizeMode:"contain"}} />
-                <Text style={styles.home}>Home</Text>
+
+                <ButtonView activeOpacity={0.8} style={styles.ButtonDropDown} onPress={() => navigation.navigate("List")}>
+                <Image style={route === "List" ? styles.homeIcon : styles.homeIconDisable} source={require('./assets/homeIcon2.png')}/>
+                <Text style={styles.homeText}>Home</Text>
                 </ButtonView>
-                <View style={styles.borderline}></View>
-                <ButtonView activeOpacity={0.8} style={{flex:0.8,flexDirection:"row",alignItems:"center"}}>
-                <Image style={styles.homeicon} source={require('./assets/group669.png')} style={{height:18, width:18, resizeMode:"contain"}}/>
-                <Text style={styles.home}>Setting</Text>
+                <View style={styles.borderLine}></View>
+
+
+                <ButtonView style={styles.ButtonDropDown}  onPress={() => navigation.navigate("List")} activeOpacity={0.8} isRound={0}>
+                    <Image style={route === "List" ? styles.homeIcon : styles.homeIconDisable} source={require('./assets/group669.png')}/>
+                    <Text style={styles.homeText}>Setting</Text>
+                    <View style={{ flex:1,alignItems:"flex-end", marginRight:5}}>
+                        <Image
+                        style={styles.dropdownIconEnable}
+                        source={require('./assets/down-arrow.png')}/>                            
+                    </View>
                 </ButtonView>
-                <View style={styles.borderline}></View>
-                <ButtonView activeOpacity={0.8} onPress={()=> navigation.navigate("Account")} style={{flex:0.8,flexDirection:"row",alignItems:"center"}}>
-                <Image style={styles.homeicon} source={require('./assets/profile.png')} style={{height:18, width:18, resizeMode:"contain"}}/>
-                <Text style={styles.home}>Account</Text>
+                <View style={styles.borderLine}></View>
+                    
+                    <ButtonView style={styles.ButtonDropDown} onPress={() => navigation.navigate("Account")} activeOpacity={0.8} isRound={0}>
+                    <Image style={route === "Account" ? styles.homeIcon : styles.homeIconDisable} source={require('./assets/profile.png')}/>
+                    <Text style={styles.homeText}>Account</Text>
+                    <View style={{ flex:1,alignItems:"flex-end", marginRight:5}}>
+                        <Image
+                        style={styles.dropdownIconEnable}
+                        source={require('./assets/down-arrow.png')}/>                            
+                    </View>
                 </ButtonView>
-                <View style={styles.borderline}></View>
-                <ButtonView activeOpacity={0.8} style={{flex:0.8,flexDirection:"row",alignItems:"center"}}>
-                <Image style={styles.homeicon} source={require('./assets/info.png')} style={{height:18, width:18, resizeMode:"contain"}}/>
-                <Text style={styles.home}>About</Text>
+                <View style={styles.borderLine}></View>
+
+                <ButtonView activeOpacity={0.8} style={styles.ButtonDropDown}>
+                <Image style={styles.homeIcon} source={require('./assets/info.png')}/>
+                <Text style={styles.homeText}>About</Text>
                 </ButtonView>
-                <View style={styles.borderline}></View>
-                <ButtonView activeOpacity={0.8} style={{flex:0.8,flexDirection:"row",alignItems:"center"}}>
-                <Image style={styles.homeicon} source={require('./assets/login.png')} style={{height:18, width:18, resizeMode:"contain"}}/>
-                <Text onPress={()=>logout()} style={styles.home}>Logout</Text>
+                <View style={styles.borderLine}></View>
+
+
+                <ButtonView activeOpacity={0.8} style={styles.ButtonDropDown}>
+                <Image style={styles.homeIcon} source={require('./assets/login.png')}/>
+                <Text onPress={()=>logout()} style={styles.homeText}>Logout</Text>
                 </ButtonView>
                 <View style={{flex:3.5}}></View>
             </View>
@@ -68,35 +89,81 @@ const styles = StyleSheet.create({
     manu:{
         flex:3,
         backgroundColor:"#ffffff",
-        paddingVertical:30,
+        paddingVertical:10,
         paddingHorizontal:20
     },
     logo: {
         height: 100,
         width: 100,
-        resizeMode:"contain"
+        resizeMode:"contain",
+        tintColor:"#fff",
       },
-    home:{
+    homeText:{
         color:"#666666",
         fontSize:16,
         fontWeight:"500",
         paddingLeft:20,
         fontFamily:"OpenSans-Bold"
-
     },
-    borderline:{
-        backgroundColor:"#e5e5e5",
-        height:1,
-        marginVertical:18
+    homeTextDisable:{
+        color:"#666666",
+        fontSize:16,
+        fontWeight:"500",
+        paddingLeft:20,
+        fontFamily:"OpenSans-Bold"
+    },
+    borderLine:{
+        backgroundColor:"#666666",
+        height:.3,
+    },
+    borderLineDisable:{
+        backgroundColor:"#666666",
+        height:.3,
     },
     groupPng:{
         width:20
     },
-    homeicon:{
+    homeIcon:{
+        height:18,
         width:18,
-        height:20,
-        tintColor:"#666666"
-    }
+        resizeMode:"contain",
+        tintColor:"#193250",        
+    },
+    homeIconDisable:{
+        height:18,
+        width:18,
+        resizeMode:"contain",
+        tintColor:"#666666",        
+    },
+    TabDropDown: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      dropdownIconDisable: {
+        width: 12,
+        height: 12,
+        resizeMode: 'contain',
+        tintColor: '#666666',
+        transform: [{rotate: '270deg'}],
+      },
+      dropdownIconEnable: {
+        width: 12,
+        height: 12,
+        resizeMode: 'contain',
+        tintColor: '#193250',
+        transform: [{rotate: '270deg'}],
+      },
+      ButtonDropDown:{
+        flexDirection:"row",
+        alignItems:"center",
+        paddingVertical:15,
+      },
+      ButtonDropDown2:{
+        flexDirection:"row",
+        alignItems:"center",
+        paddingVertical:15,
+      }
 });
 
 const mapStateToProps = state => {
